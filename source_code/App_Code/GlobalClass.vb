1640baa9919e4b7dc0d60464c5079349e0eeef51
parerajumano <48368688+parerajumano@users.noreply.github.com>
2019-04-19 18:24:28 +0700

    Add files via upload

--- source_code/App_Code/GlobalClass.vb
+++ source_code/App_Code/GlobalClass.vb
@@ -33,49 +33,27 @@ Public Module GlobalClass
         Dim bilangan As String() = {" ", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"}
 
         If nilai < 12 Then
-
             Return " " & bilangan(nilai)
-
         ElseIf nilai < 20 Then
-
             Return Terbilang(nilai - 10) & " Belas"
-
         ElseIf nilai < 100 Then
-
             Return (Terbilang(CInt((nilai \ 10))) & " Puluh") + Terbilang(nilai Mod 10)
-
         ElseIf nilai < 200 Then
-
             Return " Seratus" & Terbilang(nilai - 100)
-
         ElseIf nilai < 1000 Then
-
             Return (Terbilang(CInt((nilai \ 100))) & " Ratus") + Terbilang(nilai Mod 100)
-
         ElseIf nilai < 2000 Then
-
             Return " Seribu" & Terbilang(nilai - 1000)
-
         ElseIf nilai < 1000000 Then
-
             Return (Terbilang(CInt((nilai \ 1000))) & " Ribu") + Terbilang(nilai Mod 1000)
-
         ElseIf nilai < 1000000000 Then
-
             Return (Terbilang(CInt((nilai \ 1000000))) & " Juta") + Terbilang(nilai Mod 1000000)
-
         ElseIf nilai < 1000000000000 Then
-
             Return (Terbilang(CInt((nilai \ 1000000000))) & " Milyar") + Terbilang(nilai Mod 1000000000)
-
         ElseIf nilai < 1000000000000000 Then
-
             Return (Terbilang(CInt((nilai \ 1000000000000))) & " Trilyun") + Terbilang(nilai Mod 1000000000000)
-
         Else
-
             Return ""
-
         End If
 
     End Function
